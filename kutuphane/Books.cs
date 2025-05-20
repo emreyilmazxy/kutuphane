807aa3e02b413516286cc9085bcc9dca1afe2caf
emreyilmaz <emreyilmazxy@gmail.com>
2025-05-20 11:26:30 +0300

    Add project files.

---
+++ kutuphane/Books.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kutuphane
+{
+    public class Books
+    {
+
+        private string BookName;
+        private string Name;
+        private string Surname;
+        private string PageNumber;
+        private string Publisher;
+        private string EntryDate;
+
+        public string bookName
+        {
+            get
+            {
+                return BookName;
+            }
+            set
+            {
+                BookName = value;
+            }
+
+
+        }
+
+        public string name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+
+
+        }
+
+        public string surname
+        {
+            get
+            {
+                return Surname;
+            }
+            set
+            {
+                Surname = value;
+            }
+
+
+        }
+
+        public string pageNumber
+        {
+            get
+            {
+                return PageNumber;
+            }
+            set
+            {
+                PageNumber = value;
+            }
+
+
+        }
+
+        public string publisher
+        {
+            get
+            {
+                return Publisher;
+            }
+            set
+            {
+                Publisher = value;
+            }
+
+
+        }
+
+        public string entryDate
+        {
+            get
+            {
+                return EntryDate;
+            }
+            set
+            {
+                EntryDate = value;
+            }
+
+
+        }
+
+        public Books()
+
+        {
+            entryDate = DateTime.Today.ToShortDateString();    //parametreless constructor 
+           
+        }
+
+        public Books(string _bookName, string _name, string _surname, string _pageNumber, string _publisher)
+        {
+            bookName = _bookName;
+            name = _name;
+            surname = _surname;
+            pageNumber = _pageNumber;                                                            //parametreli constructor
+            publisher = _publisher;
+            entryDate = DateTime.Today.ToShortDateString(); 
+        }
+
+       
+
+
+    }
+}
---
+++ kutuphane/Program.cs
@@ -0,0 +1,30 @@
+﻿using kutuphane;
+
+namespace CalismaAlani
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Books book1 = new Books();
+            book1.bookName = "insanın anlam arayışı";
+            book1.name = "Viktor";
+            book1.surname = "Frankl";   //parametreless constructor
+            book1.pageNumber = "170";
+            book1.publisher = "İş Bankası Yayınları";
+            //parametreli constructor
+            Books book2 = new Books("sevme sanatı", "erik", "fromm", "200", "say yayınları");
+            Books book3 = new Books("insan olmak", "john&mary", "griffen", "408", "alfa yayınları");
+            Books book4 = new Books("Suç ve Ceza", "Fydor", "Dostoyevski", "704", "İş Bankası Yayınları");
+            Books book5 = new Books("İnsan ve Davranışı", "doğan", "cüceloğlu", "501", "remzi kitapevi");
+            Books book6 = new Books("İnsan Ne ile Yaşar", "lev", "tolstoy", "80", "bilge kültür sanat");
+
+
+            //class : alanlar ,özellikler, metodları içinde barındıran bir şablondur
+            //property : alanların özelliklerini tanımlamak için kullanılır.kapsülleme sağlar.sınıf yapısının bozulmasını önler.
+            //methodlar: sınıfın işlevselliğini artırır. belirli bir işlevi yerine getiren kod bloklarıdır.
+            //constructor: sınıfın bir örneği oluşturulduğunda çalışan özel bir metoddur. sınıfın özelliklerini başlatmak için kullanılır.varsayılan değerleri atar.
+        }
+    }
+}
